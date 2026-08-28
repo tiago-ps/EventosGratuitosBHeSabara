8d91327b9d75d4423ee62eb6a2862ca1cbc4205a
UmTremdeCinemaIFMG <umtremdecinema@ifmg.edu.br>
2026-08-28 02:21:04 -0300

    feat: alterna perfil tematico pelo banner

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mural-cultural-v95-check-verde';
+const CACHE_VERSION = 'mural-cultural-v96-toggle-perfil-tematico';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
@@ -16,8 +16,8 @@ const CORE_ASSETS = [
   './js/conteudos/cursos.js?v=2',
   './js/conteudos/concursos.js?v=2',
   './js/conteudos/filmes.js?v=5',
-  './js/app.js?v=86',
-  './js/temas-visuais.js?v=3',
+  './js/app.js?v=87',
+  './js/temas-visuais.js?v=4',
   './js/eventos-manuais-ui.js?v=44',
   './js/ios-install.js?v=1',
   './imagens/curadorias/agosto-lilas-banner.png',
