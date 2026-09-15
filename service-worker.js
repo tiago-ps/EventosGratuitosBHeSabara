0b1f00164717e7f006b998f7f650561c8a45a702
Tiago Pereira da Silva <88672838+tiago-ps@users.noreply.github.com>
2026-09-14 23:50:14 -0300

    chore(pwa): atualiza cache da pesquisa da agenda

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mural-cultural-v117-painel-navegacao';
+const CACHE_VERSION = 'mural-cultural-v118-agenda-pesquisa-foco';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
@@ -25,6 +25,7 @@ const CORE_ASSETS = [
   './js/eventos-manuais-ui.js?v=44',
   './js/ios-install.js?v=2',
   './js/painel-navegacao-modos.js?v=1',
+  './js/agenda-pesquisa-foco.js?v=1',
   './js/painel-acoes-contextuais.js?v=2',
   './js/cursos-runtime-fix.js?v=2',
   './imagens/curadorias/agosto-lilas-banner.png',
@@ -67,7 +68,7 @@ async function trimCache(cacheName, maxItems) {
   const cache = await caches.open(cacheName);
   const keys = await cache.keys();
   const excess = keys.length - maxItems;
-  if (excess > 0) await Promise.all(keys.slice(0, excess).map(key => cache.delete(key)));
+  if (excess > 0) await Promise.all(keys.slice(0, excess).map(key => caches.delete(key)));
 }
 
 async function networkFirst(request, cacheName, fallbackUrl = '', expectedContentType = '') {
