2e9452f8dc6a15cffda3e95d1b60a6b93bf03a76
Tiago Pereira da Silva <88672838+tiago-ps@users.noreply.github.com>
2026-09-21 00:09:14 -0300

    chore: renova cache do tema claro da agenda

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mural-cultural-v126-agenda-header';
+const CACHE_VERSION = 'mural-cultural-v127-agenda-theme';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
@@ -8,7 +8,7 @@ const CURATION_IMAGE_PREFIX = '/imagens/curadorias/';
 
 const CORE_ASSETS = [
   './', './index.html',
-  './css/styles.css?v=76',
+  './css/styles.css?v=77',
   './css/eventos-manuais-ui.css?v=43',
   './css/concursos-mural.css?v=3',
   './css/temas-visuais.css?v=11',
@@ -21,7 +21,7 @@ const CORE_ASSETS = [
   './js/conteudos/concursos.js?v=2',
   './js/conteudos/filmes.js?v=6',
   './js/curadorias-site.js?v=11',
-  './js/app.js?v=102',
+  './js/app.js?v=103',
   './js/temas-visuais.js?v=12',
   './js/eventos-manuais-ui.js?v=44',
   './js/ios-install.js?v=2',
