ff60be43571eb1833b71c0814498baaa4a845dee
Tiago Pereira da Silva <88672838+tiago-ps@users.noreply.github.com>
2026-09-21 22:42:39 -0300

    chore: renova cache da compactacao de acervos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mural-cultural-v134-correct-tem-sim-uai-assets';
+const CACHE_VERSION = 'mural-cultural-v132-book-holdings-all-screens';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
@@ -8,7 +8,7 @@ const CURATION_IMAGE_PREFIX = '/imagens/curadorias/';
 
 const CORE_ASSETS = [
   './', './index.html',
-  './css/styles.css?v=80',
+  './css/styles.css?v=81',
   './css/eventos-manuais-ui.css?v=43',
   './css/concursos-mural.css?v=3',
   './css/temas-visuais.css?v=11',
